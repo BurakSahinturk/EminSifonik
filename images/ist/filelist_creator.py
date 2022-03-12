1abb6d5667bc99c0b94518747553cfbb6b80ac34
Burak Şahintürk <buraksahinturk@gmail.com>
2022-03-12 12:56:50 +0300

    Updates

--- images/ist/filelist_creator.py
+++ images/ist/filelist_creator.py
@@ -5,14 +5,58 @@ Created on Thu Mar 10 13:54:31 2022
 @author: Besteq
 """
 
+
 import os
+
+dir= input("Working Directory name: ")
+projectpath = input("Upper Path: ")
 dir_list = os.listdir()
-f = open("filelist.js", "w")
-f.write("{\n")
-for x in dir_list:
-    if x == "filelist.js" or x == "filelist_creator.py":
+index = 1
+f = open("filelist.jsx", "w")
+
+openingText = """
+import React, { Component } from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';\n"""
+
+
+f.write(openingText)
+f.close()
+f = open("filelist.jsx", "a")
+
+for fname in dir_list:
+    if fname == "filelist.jsx" or fname == "filelist_creator.py":
         continue
     else:
-        f.write('\t"' + x +'"\n') 
-f.write("}")
+        f.write("import CarouselPic" + str(index) + " from '" + projectpath + "/" + dir + "/" + fname + "';\n")
+        index = index + 1
+
+dirC = dir.capitalize()
+
+f.write(""" 
+        
+
+export default class """ + dirC + """Carousel extends Component {
+    render() {
+        return (
+            <Carousel autoPlay={true} infiniteLoop={true}>""")
+index = 1
+
+for fname in dir_list:
+    if fname == "filelist.jsx" or fname == "filelist_creator.py":
+        continue
+    else:
+        f.write("""
+                <div>
+                    <img src={CarouselPic""" + str(index) + """}/>
+                    <p className="legend">Legend</p>
+                </div>
+ """)
+        index = index + 1
+
+f.write("""
+            </Carousel>
+        );
+    }
+};""")
 f.close()
